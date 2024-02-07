b5150f47487ef5aec09a5229f863b7a70f8e059c
Michael De Maegt <mda65bb@admin.colruyt.int>
2024-02-07 19:44:26 +0100

    Corrected some conversion of the dataset. Still some , instead of . so, expect trouble

--- R/Extract XTRA info.R
+++ R/Extract XTRA info.R
@@ -2,6 +2,51 @@ source("R/Load Libraries.R")
 source("R/References.R")
 
 
+get_xtra_data <- function(file_name, convert_comma=FALSE) {
+  
+  xtra_raw_data <- 
+    fread(file_name) %>%
+    tibble() %>%
+    clean_names()
+  
+  if(convert_comma) {
+    xtra_raw_data <-
+      xtra_raw_data %>%
+      mutate(turnoverexclvat=str_replace(turnoverexclvat,",","\\.")) %>% 
+      mutate(turnoverinclvat=str_replace(turnoverinclvat,",","\\.")) %>%
+      mutate(turnover_costprice_excl_vat=
+               str_replace(turnover_costprice_excl_vat,",","\\.")) %>% 
+      mutate(vatpercentage=str_replace(vatpercentage,",","\\.")) %>% 
+      mutate(grossmargin_brutowinst=str_replace(grossmargin_brutowinst,",","\\.")) %>%
+      mutate(cost_price=str_replace(cost_price,",","\\."))
+  }
+    
+  xtra_raw_data <-
+    xtra_raw_data %>%
+    # Remove the empty sales lines
+    filter(turnoverexclvat>0) %>%
+    
+    # Clean-up the data
+    mutate(sales_date = as.Date(sales_date)) %>%
+    mutate(plantname=str_to_upper(plantname)) %>%
+    rename(shop=plantname) %>%
+    rename(shop_nr = plant) %>%
+    rename(grossmargin=grossmargin_brutowinst,
+           identifier=herekenning) %>%
+    mutate(shop = str_trim(str_remove(shop,"DREAMBABY"),"both")) %>%
+    mutate(grossmargin = as.numeric(grossmargin),
+           turnover_costprice_excl_vat = as.numeric(turnover_costprice_excl_vat)) %>%
+    mutate(turnoverexclvat = as.numeric(turnoverexclvat) ,
+           turnoverinclvat = as.numeric(turnoverinclvat)) %>%
+
+    # Add the zipcode that is associated with the cbh_id
+    left_join(cbh_id_raw_data,by = join_by(cbh_id)) %>%
+    select(-producthierarchy) %>%
+    left_join(bd_raw,by = join_by(cbh_id))
+  
+  return(  xtra_raw_data )
+}
+
 bd_raw <- 
   read_feather("../Feather Files/cbh_id_birth_date.feather") 
 
@@ -22,34 +67,28 @@ write_feather(cbh_id_raw_data,"../Feather Files/cbh_id2zipcode.feather")
 
 
 ###############
-file_name <- "Data/Dreambaby_Xtra_v2.csv"
 
+xtra_raw_data_p1 <-
+  get_xtra_data("Data/Dreambaby_Xtra_v2.csv") %>%
+  filter(sales_date < ymd("2023-11-02")) %>%
+  mutate(product=as.integer(product))
 
-xtra_raw_data <- 
-  fread(file_name) %>%
-  tibble() %>%
-  clean_names() %>%
-  
-  # Remove the empty sales lines
-  filter(turnoverexclvat>0) %>%
-  
-  # Clean-up the data
-  mutate(sales_date = as.Date(sales_date)) %>%
-  mutate(plantname=str_to_upper(plantname)) %>%
-  rename(shop=plantname) %>%
-  rename(shop_nr = plant) %>%
-  rename(grossmargin=grossmargin_brutowinst,
-         identifier=herekenning) %>%
-  mutate(shop = str_trim(str_remove(shop,"DREAMBABY"),"both")) %>%
-  mutate(grossmargin = as.numeric(grossmargin),
-         turnover_costprice_excl_vat = as.numeric(turnover_costprice_excl_vat)) %>%
-  
-  # Add the zipcode that is associated with the cbh_id
-  left_join(cbh_id_raw_data,by = join_by(cbh_id)) %>%
-  select(-producthierarchy) %>%
-  left_join(bd_raw,by = join_by(cbh_id))
-  
-
+xtra_raw_data_p2 <- 
+  get_xtra_data("Data/dm_storecheckoutsales_DB_202311_202311.csv",
+                convert_comma=TRUE) %>%
+  rename(postcode=postcode.x) %>%
+  select(-postcode.y) %>%
+  mutate(vatpercentage = as.numeric(vatpercentage)) %>%
+  mutate(totalowndiscount = as.numeric(totalowndiscount)) %>%
+  mutate(totaldiscountamount = as.numeric(totaldiscountamount)) %>%
+  mutate(discountpercentage = as.numeric(discountpercentage)) %>%
+  mutate(supplierdiscount = as.numeric(supplierdiscount)) %>%
+  mutate(owndiscount = as.numeric(owndiscount)) %>%
+  mutate(publidiscount = as.numeric(publidiscount)) %>%
+  mutate(globalmanualdiscount = as.numeric(globalmanualdiscount))
+ 
+xtra_raw_data <-
+  bind_rows(xtra_raw_data_p1,xtra_raw_data_p2) 
 
 # Save data to feather
 write_feather(xtra_raw_data,"../Feather Files/Sales XTRA file.feather")
@@ -58,21 +97,10 @@ write_feather(xtra_raw_data,"../Feather Files/Sales XTRA file.feather")
 if(FALSE)
 {
 
-  mdm <-
-    xtra_raw_data %>%
-    select(sales_date,cbh_id, zipcode) %>%
-    filter(is.na(zipcode)) %>%
-    filter(!is.na(cbh_id)) %>%
-    select(-zipcode) %>%
-    group_by(cbh_id) %>%
-    mutate(first = min(sales_date)==sales_date) %>%
-    ungroup() %>% 
-    filter(first) %>%
-    select(-first) %>%
-    unique()
+  xtra_raw_data_p1$vatpercentage[1]
+  xtra_raw_data_p2$vatpercentage[1]
   
-  
-  cbh_id_raw_data %>%
-    filter(cbh_id==22607473)
+  xtra_raw_data <-  xtra_raw_data %>%
+    arrange(sales_date)
   
 }
